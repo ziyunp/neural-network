f529528714ea3515f113964fb7d6488112850da4
Zijian_Ou <kenou1996@126.com>
2020-02-12 12:05:29 +0000

    refac: comment example main

--- nn_lib.py
+++ nn_lib.py
@@ -261,7 +261,7 @@ class MultiLayerNetwork(object):
         #######################################################################
         self._layers = []
         input_n = input_dim
-        for i in len(neurons):
+        for i in range(len(neurons)):
             self._layers.append(LinearLayer(input_n, neurons[i]))
             if activations[i] == "relu":
                 self._layers.append(ReluLayer())
@@ -535,48 +535,48 @@ class Preprocessor(object):
         #######################################################################
 
 
-def example_main():
-    input_dim = 4
-    neurons = [16, 3]
-    activations = ["relu", "identity"]
-    net = MultiLayerNetwork(input_dim, neurons, activations)
+# def example_main():
+#     input_dim = 4
+#     neurons = [16, 3]
+#     activations = ["relu", "identity"]
+#     net = MultiLayerNetwork(input_dim, neurons, activations)
 
-    dat = np.loadtxt("iris.dat")
-    np.random.shuffle(dat)
+#     dat = np.loadtxt("iris.dat")
+#     np.random.shuffle(dat)
 
-    x = dat[:, :4]
-    y = dat[:, 4:]
+#     x = dat[:, :4]
+#     y = dat[:, 4:]
 
-    split_idx = int(0.8 * len(x))
+#     split_idx = int(0.8 * len(x))
 
-    x_train = x[:split_idx]
-    y_train = y[:split_idx]
-    x_val = x[split_idx:]
-    y_val = y[split_idx:]
+#     x_train = x[:split_idx]
+#     y_train = y[:split_idx]
+#     x_val = x[split_idx:]
+#     y_val = y[split_idx:]
 
-    prep_input = Preprocessor(x_train)
+#     prep_input = Preprocessor(x_train)
 
-    x_train_pre = prep_input.apply(x_train)
-    x_val_pre = prep_input.apply(x_val)
+#     x_train_pre = prep_input.apply(x_train)
+#     x_val_pre = prep_input.apply(x_val)
 
-    trainer = Trainer(
-        network=net,
-        batch_size=8,
-        nb_epoch=1000,
-        learning_rate=0.01,
-        loss_fun="cross_entropy",
-        shuffle_flag=True,
-    )
+#     trainer = Trainer(
+#         network=net,
+#         batch_size=8,
+#         nb_epoch=1000,
+#         learning_rate=0.01,
+#         loss_fun="cross_entropy",
+#         shuffle_flag=True,
+#     )
 
-    trainer.train(x_train_pre, y_train)
-    print("Train loss = ", trainer.eval_loss(x_train_pre, y_train))
-    print("Validation loss = ", trainer.eval_loss(x_val_pre, y_val))
+#     trainer.train(x_train_pre, y_train)
+#     print("Train loss = ", trainer.eval_loss(x_train_pre, y_train))
+#     print("Validation loss = ", trainer.eval_loss(x_val_pre, y_val))
 
-    preds = net(x_val_pre).argmax(axis=1).squeeze()
-    targets = y_val.argmax(axis=1).squeeze()
-    accuracy = (preds == targets).mean()
-    print("Validation accuracy: {}".format(accuracy))
+#     preds = net(x_val_pre).argmax(axis=1).squeeze()
+#     targets = y_val.argmax(axis=1).squeeze()
+#     accuracy = (preds == targets).mean()
+#     print("Validation accuracy: {}".format(accuracy))
 
 
-if __name__ == "__main__":
-    example_main()
+# if __name__ == "__main__":
+#     example_main()
