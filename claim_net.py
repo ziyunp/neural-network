67c2ad6275e1e3daf0cb5cba643285fc224c96ab
Zijian_Ou <kenou1996@126.com>
2020-02-19 13:31:47 +0000

    feat: create a class for network (untested)

---
+++ claim_net.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ClaimNet(nn.Module):
+
+    def __init__(self, input_dim, neurons, activations, loss_fun):
+        """
+        Feel free to alter this as you wish, adding instance variables as
+        necessary. 
+        """
+        super(ClaimNet, self).__init__()
+
+        self._layers = []
+        n_inputs = input_dim
+        for i in range(len(neurons)):
+            self._layers.append(nn.Linear(n_inputs, neurons[i]))
+            if activations[i] == "relu":
+                self._layers.append(nn.ReLU())
+            elif activations[i] == "sigmoid":
+                self._layers.append(nn.Sigmoid())
+            elif activations[i] == "softmax":
+                self._layers.append(nn.Softmax(dim=1))
+            elif activations[i] == "tanh":
+                self._layers.append(nn.Tanh())
+            n_inputs = neurons[i]  
+
+    def forward(self, x):
+        """
+        Args:
+            x (Tensor) : attributes
+        """
+        for layer in self._layers:
+            x = layer(x)
+        return x
