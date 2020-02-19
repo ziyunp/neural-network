0f88350a74ae18fd5a04fb41911ec13b0282f557
Zijian_Ou <kenou1996@126.com>
2020-02-19 14:38:03 +0000

    feat: instead of producing dictionary, produce two numpy arrays

--- claim_dataset.py
+++ claim_dataset.py
@@ -8,25 +8,25 @@ class ClaimDataset(Dataset):
     _ATTRIBUTE_NUM = 9
     _LABEL_IDX = 9
 
-    def __init__(self, npdata):
+    def __init__(self, attributes, labels):
         """
         Args:
             npdata (ndarray): Dataset stored in a numpy array. 
         """
         # drv_age1, vh_age, vh_cyl, vh_din, pol_bonus, vh_sale_begin, vh_sale_end, 
         # vh_value, vh_speed, claim_amount, made_claim
-        self.dataset = npdata
+        self._attributes = attributes
+        self._labels = labels
 
     def __len__(self):
-        return len(self.dataset)
+        return len(self._attributes)
 
     def __getitem__(self, index):
         # if the index is a vector, convert it into a list
         if torch.is_tensor(index):
             index = index.tolist()
 
-        attributes = self.dataset[index, :self._ATTRIBUTE_NUM]
-        label = self.dataset[index, self._LABEL_IDX:]
-        sample = {'attributes' : attributes, 'label' : label}
+        ret_attributes = self._attributes[index]
+        ret_labels = self._labels[index]
 
-        return sample
+        return ret_attributes, ret_labels
