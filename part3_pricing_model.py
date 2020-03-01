0cce63919d40e2044dd70261e7fb5f9630a1f042
Ziyunp <ziyun_poh@hotmail.com>
2020-03-01 16:58:39 +0000

    both predict methods working

--- part3_pricing_model.py
+++ part3_pricing_model.py
@@ -178,10 +178,10 @@ class PricingModel():
         x_val_clean = self._preprocessor(x_val)
         # THE FOLLOWING GETS CALLED IF YOU WISH TO CALIBRATE YOUR PROBABILITES
         if self.calibrate:
-            self.base_classifier = fit_and_calibrate_classifier(
+            fit_and_calibrate_classifier(
                 self.base_classifier, X_clean, y_clean, x_val_clean, y_val, early_stop)
         else:
-            self.base_classifier = self.base_classifier.fit(X_clean, y_clean, x_val_clean, y_val, early_stop)
+            self.base_classifier.fit(X_clean, y_clean, x_val_clean, y_val, early_stop)
         return self.base_classifier
 
     def predict_claim_probability(self, X_raw):
@@ -203,10 +203,10 @@ class PricingModel():
         """
         # =============================================================
         # REMEMBER TO A SIMILAR LINE TO THE FOLLOWING SOMEWHERE IN THE CODE
-        # X_clean = self._preprocessor(X_raw)
+        X_clean = self._preprocessor(X_raw)
+        y_pred = self.base_classifier.predict(X_clean)
 
-
-        return  # return probabilities for the positive class (label 1)
+        return  y_pred # return probabilities for the positive class (label 1)
 
     def predict_premium(self, X_raw):
         """Predicts premiums based on the pricing model.
@@ -237,6 +237,8 @@ class PricingModel():
         with open('part3_pricing_model.pickle', 'wb') as target:
             pickle.dump(self, target)
 
+    def evaluate_architecture(self, probability, annotation):
+        self.base_classifier.evaluate_architecture(probability, annotation)
 
 def load_model():
     # Please alter this section so that it works in tandem with the save_model method of your class
@@ -295,41 +297,32 @@ def main():
     loss_fun = "bce"
     optimiser = "sgd"
     learning_rate = 1e-3
-    epoch = 10
+    epoch = 100
     batch_size = 1000
 
     model = PricingModel(input_dim, output_dim, neurons, activations, loss_fun, optimiser, learning_rate, epoch, batch_size, False)
 
     # Train the network
-    loss_hist, loss_val_hist, roc_auc_hist = \
-        model.fit(x_train, y_train, claim_amount, x_val, y_val, 0.00008)
+    model.fit(x_train, y_train, claim_amount, x_val, y_val, 0.00008)
     
-    plt.figure(figsize=(6, 5))
-    plt.xlabel("Epoch", fontsize=16)
-    plt.plot(loss_hist, label='training loss')
-    plt.plot(loss_val_hist, label='validation loss')
-    plt.plot(roc_auc_hist, label='ROC AUC')
-    plt.legend()
-    plt.show()
+    # plt.figure(figsize=(6, 5))
+    # plt.xlabel("Epoch", fontsize=16)
+    # plt.plot(loss_hist, label='training loss')
+    # plt.plot(loss_val_hist, label='validation loss')
+    # plt.plot(roc_auc_hist, label='ROC AUC')
+    # plt.legend()
+    # plt.show()
+
 #     claim_classifier.save_model()
 
-#     #Predict
-#     prob_train = claim_classifier.predict(x_train)
-
-#     # Evaluation
-#     print()
-#     print("------- The result of training set is: ------")
-#     claim_classifier.evaluate_architecture(prob_train, y_train)
-
-#     #Predict for validation
-#     prob_val = claim_classifier.predict(x_val)
-
-#     # Evaluation for validation
-#     print()
-#     print("------- The result of validation set is: ------")
-#     claim_classifier.evaluate_architecture(prob_val, y_val)
-
-#     plot_precision_recall(prob_val, y_val)
+    #Predict
+    prob_train = model.predict_claim_probability(x_test)
+    premium = model.predict_premium(x_test)
+    print("premium: ", premium)
+    # Evaluation
+    print()
+    print("------- The result of training set is: ------")
+    model.evaluate_architecture(prob_train, y_test)
 
 
 
